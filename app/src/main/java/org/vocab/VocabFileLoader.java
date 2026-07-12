d2cf2c113edcc3e9fd9b1d6394400d5efd174dd0
iasonas <iasonas.tan@gmail.com>
2026-07-12 17:43:56 +0300

    Fixed issue #1, Added test file.

--- app/src/main/java/org/vocab/VocabFileLoader.java
+++ app/src/main/java/org/vocab/VocabFileLoader.java
@@ -1,24 +1,23 @@
 package org.vocab;
 
-import org.vocab.Vocabulary;
-
-import java.util.Properties;
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public final class VocabFileLoader {
-    private Vocabulary vocabulary;
+    private final Vocabulary vocabulary;
 
     public VocabFileLoader(Vocabulary vocab) {
         vocabulary = vocab;
     }
 
     public void loadVocab(InputStreamReader reader) throws IOException {
-        Properties properties = new Properties();
-        properties.load(reader);
-        properties.forEach((k, v) -> {
-            vocabulary.add((String)k, (String)v);
-        });
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        while((line = bufferedReader.readLine()) != null) {
+            String[] lineSplit = line.split("=");
+            vocabulary.add(lineSplit[0], lineSplit[1]);
+            IO.println("Pair: " + lineSplit[0] + " === " + lineSplit[1]);
+        }
     }
 }
