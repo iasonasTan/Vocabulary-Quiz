768fefab1301f6e99fe1d41f6c571081545c84d5
iasonas <iasonas.tan@gmail.com>
2026-07-29 22:31:45 +0300

    Updated download-jfx.bat

--- release-windows/download-jfx.bat
+++ release-windows/download-jfx.bat
@@ -4,7 +4,9 @@ set "INSTALL_DIR=jfx-windows"
 set "TEMP_FILE=%TEMP%\openjfx.zip"
 set "EXTRACT_TEMP=%TEMP%\javafx_extracted"
 
-if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
+if exist "%INSTALL_DIR%" rmdir /s /q "%INSTALL_DIR%"
+mkdir "%INSTALL_DIR%"
+
 if exist "%EXTRACT_TEMP%" rmdir /s /q "%EXTRACT_TEMP%"
 mkdir "%EXTRACT_TEMP%"
 
@@ -14,7 +16,9 @@ curl -L -o "%TEMP_FILE%" "%URL%"
 echo Unzipping javafx sdk...
 tar -xf "%TEMP_FILE%" -C "%EXTRACT_TEMP%"
 
-for /d %%i in ("%EXTRACT_TEMP%\*") do move "%%i\*" "%INSTALL_DIR%\"
+for /d %%d in ("%EXTRACT_TEMP%\*") do (
+    xcopy /E /Y "%%d\*" "%INSTALL_DIR%\"
+)
 
 del /f /q "%TEMP_FILE%"
 rmdir /s /q "%EXTRACT_TEMP%"
