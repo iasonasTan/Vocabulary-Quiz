fd537f78332ef60827513dd6c8371852746bc14f
Jason Tantaros <162042820+iasonasTan@users.noreply.github.com>
2026-07-29 20:43:20 +0300

    Add script to download and extract JavaFX SDK

---
+++ release-windows/download-jfx.bat
@@ -0,0 +1,17 @@
+$URL = "https://download.java.net/java/GA/javafx25.0.2/1ccc8ce474414c94ade008f8833286e8/4/openjfx-25.0.2_windows-x64_bin-sdk.zip"
+$INSTALL_DIR = "jfx-windows"
+$TEMP_FILE = "$env:TEMP\openjfx.zip"
+
+if (!(Test-Path $INSTALL_DIR)) {
+    New-Item -ItemType Directory -Force -Path $INSTALL_DIR | Out-Null
+}
+
+Write-Host "Downloading javafx sdk..."
+Invoke-WebRequest -Uri $URL -OutFile $TEMP_FILE
+
+Write-Host "Unzipping javafx sdk..."
+tar -xzf $TEMP_FILE -C $INSTALL_DIR --strip-components=1
+
+Remove-Item $TEMP_FILE
+
+Write-Host "JavaFX downloaded successfuly!"
